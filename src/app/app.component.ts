d8b1aa3d6e59c5a41c55a0148f02b6004e521cd0
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2023-02-13 12:14:14 +0530

    firebase crud with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
-
+import { Firestore,collection,addDoc,collectionData,doc,updateDoc, deleteDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud';
+
+  userData !: Observable <any>
+
+  constructor(private firestore: Firestore) { 
+    this.getData()
+  }
+
+  addData(f:any) {
+    //console.log(f.value);
+    const userCollection = collection(this.firestore, 'users')
+    addDoc(userCollection, f.value).then(() => { 
+      console.log("Data added successfully")
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
+
+  getData() {
+    const userCollection = collection(this.firestore, 'users')
+    collectionData(userCollection,{idField: 'id'}).subscribe(data => {
+      console.log(data);
+    })
+    this.userData = collectionData(userCollection,{idField: 'id'})
+
+  }
+
+  updateData(id:any) {
+    const docCollection = doc(this.firestore, 'users', id)
+    const updateD = {
+      name: 'World',
+    }
+    updateDoc(docCollection, updateD).then(() => {
+      console.log("success")
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  deteleData(id: any) {
+    const userCollection = doc(this.firestore, 'users',id)
+    deleteDoc(userCollection).then(() => {
+      console.log("data deleted");
+    }).catch((err) => { 
+      console.log(err);
+    })
+  }
+
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-form-5b731',
+    appId: '1:766690134461:web:7bb6332cf543249cd3aad4',
+    storageBucket: 'angular-form-5b731.appspot.com',
+    apiKey: 'AIzaSyD-RVZDN4YRHaPl25EVecxi5tzKoXy7y7c',
+    authDomain: 'angular-form-5b731.firebaseapp.com',
+    messagingSenderId: '766690134461',
+    measurementId: 'G-DH2LWWQ5G5',
+  },
+    production: false
+};
